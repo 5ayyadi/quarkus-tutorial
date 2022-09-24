16d7e0e582eefecb29f64220ba6f6658e08fe1ba
Ali Sayyadi <as.alisayyadi@gmail.com>
2022-09-24 18:51:21 +0330

    Added token details and balance of

--- src/main/java/com/core/network/NetworkConfig.java
+++ src/main/java/com/core/network/NetworkConfig.java
@@ -2,6 +2,11 @@ package com.core.network;
 
 import org.web3j.abi.datatypes.Address;
 
+import com.gs.Token;
+
+import io.quarkus.logging.Log;
+
+import java.math.BigInteger;
 import java.util.List;
 
 public class NetworkConfig {
@@ -11,6 +16,24 @@ public class NetworkConfig {
 
     public NetworkConfig() {
 
+    }
+
+    public static Token tokenDetails(Address address, String RPC, String PrivateKey){
+        Token token = new Token(address.toString(),RPC,PrivateKey);
+        return token;
+    }
+
+    // TODO: Big integer to change to decimal
+    public BigInteger getBalance(Address address, String RPC, String PrivateKey){
+        Token token = new Token(address.toString(),RPC , PrivateKey);
+        String owner = ""; // = getPublicKey(PrivateKey) // TODO: some function that converts private key to public
+        try{
+            return token.contract.balanceOf(owner).send();
+        }
+        catch(Exception e){
+            Log.errorf("NetworkConfig at getBalance, Exception: ", e);
+            return null;
+        }
     }
 
 }
--- src/main/java/com/gs/Token.java
+++ src/main/java/com/gs/Token.java
@@ -1,17 +1,38 @@
 package com.gs;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import org.web3j.contracts.token.ERC20BasicInterface;
-import org.web3j.contracts.token.ERC20Interface;
+import io.quarkus.logging.Log;
 
-import java.math.BigDecimal;
+
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.DefaultGasProvider;
+
+
 
 public class Token extends PanacheEntity {
     public String name;
     public String symbol;
     public String address;
-
+    public ERC20 contract;
     public int decimals;
+
+    public Token(String address, String RPC, String PK){
+        this.address = address;
+        Web3j web3j = Web3j.build(new HttpService(RPC));
+        Credentials creds = Credentials.create(PK);
+        contract = new ERC20(address, web3j, creds, new DefaultGasProvider());
+        try{
+            symbol = contract.symbol().send();
+            name = contract.name().send();
+            decimals = contract.decimals().send().intValue();
+        }
+        catch(Exception e) {
+            Log.errorf("Error at line 35. Exception: ", e);
+        }
+
+    }
 
     /*
      * amount is an
