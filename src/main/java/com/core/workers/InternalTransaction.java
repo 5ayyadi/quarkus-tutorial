35fb52aba09b2faaf7c530bc7b997742921fbc71
Ali Sayyadi <as.alisayyadi@gmail.com>
2022-09-28 13:59:17 +0330

    transaction worker

---
+++ src/main/java/com/core/workers/InternalTransaction.java
@@ -0,0 +1,33 @@
+package com.core.workers;
+
+import java.util.List;
+
+import com.core.models.TransactionStatus;
+import com.core.models.TransactionType;
+import com.core.wallet.WalletInternalTransactions;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+public class InternalTransaction extends PanacheEntity {
+
+    public void confirmedTransactions(){
+        List<WalletInternalTransactions> confirmedTransactions = 
+        WalletInternalTransactions.findByStatus(TransactionStatus.CONFIRMED);
+        for(WalletInternalTransactions trx: confirmedTransactions){
+            switch(trx.trxType){
+                case TRANSFER:
+                // add balance to destination
+                // subtract from source
+                break;
+                case DEPOSIT:
+                // add balance to destination
+                break;
+                case WITHDRAW:
+                // subtract balance from source
+            }
+            // change status to success
+        }
+
+    }
+    
+}
